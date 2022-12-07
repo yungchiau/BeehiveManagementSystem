4e0c9a65b41ef6b0547a2d364e679155a5ff9fab
YungChiau <chiau8526g@gmail.com>
2022-12-07 21:58:42 +0800

    Add project files.

---
+++ BeehiveManagementSystem/Queen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    internal class Queen
+    {
+       /// <summary>
+       /// Expand worker array and add a Bee reference
+       /// </summary>
+       /// <param name="worker"> A bee which will add in worker array</param>
+       /// 
+
+        private void AddWorker(Bee worker)
+        {
+            if (unassignedWorkers >= 1)
+            {
+                unassiginedWorkers--;
+                Array.Resize(ref workers, workers.Length + 1);
+                workers[workers.Length - 1] = worker;
+            }
+        }
+
+    }
+}
