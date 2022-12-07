25bd6fb53cb3dadacbf2cd9c2e6d6dbd9bad82f6
YungChiau <chiau8526g@gmail.com>
2022-12-07 22:33:46 +0800

    Add HoneyVault class due to this class is indepent to other classes.

---
+++ BeehiveManagementSystem/HoneyVault.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    internal class HoneyVault
+    {
+        public static string[] HoneyValut(float honey, int nectar) 
+        { 
+        
+        }
+
+
+        private void CollectNectat(float amount)
+        {
+
+        }
+        private void ConvertNectarToHoney(float amount)
+        {
+
+        }
+        private void StatusReport()
+        {
+
+        }
+
+    }
+}
