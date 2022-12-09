6d46b1cb7f0fef119948e56dbc7445eaf2964dde
YungChiau <chiau8526g@gmail.com>
2022-12-09 15:37:41 +0800

    Built static HoneyVault

--- BeehiveManagementSystem/HoneyVault.cs
+++ BeehiveManagementSystem/HoneyVault.cs
@@ -3,28 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace BeehiveManagementSystem
 {
-    internal class HoneyVault
+    internal static class HoneyVault
     {
-        public static string[] HoneyValut(float honey, int nectar) 
-        { 
         
+        public const float NECTAR_CONVERSION_RATIO = .19f;
+        public const float LOW_LEVEL_WARNING = 10f;
+        private static float honey = 25f;
+        private static float nectar = 100f;
+        
+
+
+        public static void CollectNectar(float amount)
+        {
+            if (amount > 0f) nectar += amount;
+        }
+        public static void ConvertNectarToHoney(float amount)
+        {
+            float nectarToConvert = amount;
+            if (nectarToConvert > nectar) nectarToConvert =nectar;
+            nectar -= nectarToConvert;
+            honey += (nectarToConvert * NECTAR_CONVERSION_RATIO);
+       
         }
 
-
-        private void CollectNectat(float amount)
+        public static bool ConsumeHoney(float amount, float honey)
         {
+            if( honey >= amount)
+            {
+                honey -= amount;
+                return true;
+            }
+            return false;
 
         }
-        private void ConvertNectarToHoney(float amount)
+        public static string StatusReport
         {
-
-        }
-        private void StatusReport()
-        {
-
+            get {
+                string status = $"{honey:0.0} units of honey\n" +
+                                $"{nectar:0.0} units of nectar";
+                string warnings = "";
+                if (honey < LOW_LEVEL_WARNING) warnings += "\nLOW HONEY - ADD A HONEY MANUFACTURER";
+                if (nectar < LOW_LEVEL_WARNING) warnings += "\nLOW NECTAR - ADD A NECTAR COLLECTOR";
+                return status + warnings;
+            }
         }
 
     }
